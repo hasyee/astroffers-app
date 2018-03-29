1e89914ee72327301f26dab09616b610de50d046
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2018-03-29 11:04:56 +0200

    improve worker cid

--- src/api.ts
+++ src/api.ts
@@ -3,18 +3,15 @@ import { Filter, Result } from './types';
 import { v1 as uuid } from 'uuid';
 
 const worker = new Thread('worker.js');
-const cidMap = new Map();
+let cid = null;
 
 export const filterObjects = (filter: Filter): Promise<Result> =>
   new Promise((resolve, reject) => {
-    const cid = uuid();
-    const handler = message => {
+    cid = uuid();
+    worker.onmessage = message => {
       const response = JSON.parse(message);
       if (response.cid !== cid) return;
-      cidMap.delete(cid);
       resolve(response.result);
     };
-    cidMap.set(cid, handler);
-    worker.onmessage = handler;
     worker.postMessage(JSON.stringify({ cid, filter }));
   });
