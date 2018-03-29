0d2062820d116c4463a19f9a8f5ccb924335b2e6
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2018-03-29 10:57:27 +0200

    worker with cid

--- src/api.ts
+++ src/api.ts
@@ -1,18 +1,20 @@
 import { Thread } from 'react-native-threads';
 import { Filter, Result } from './types';
+import { v1 as uuid } from 'uuid';
 
 const worker = new Thread('worker.js');
-let isWorkerBusy = false;
+const cidMap = new Map();
 
 export const filterObjects = (filter: Filter): Promise<Result> =>
   new Promise((resolve, reject) => {
-    if (isWorkerBusy) return reject(new Error('Worker is busy'));
-    isWorkerBusy = true;
-    worker.onmessage = message => {
-      if (!isWorkerBusy) return reject(new Error('Worker is already done'));
-      const data = JSON.parse(message);
-      isWorkerBusy = false;
-      resolve(data);
+    const cid = uuid();
+    const handler = message => {
+      const response = JSON.parse(message);
+      if (response.cid !== cid) return;
+      cidMap.delete(cid);
+      resolve(response.result);
     };
-    worker.postMessage(JSON.stringify(filter));
+    cidMap.set(cid, handler);
+    worker.onmessage = handler;
+    worker.postMessage(JSON.stringify({ cid, filter }));
   });
